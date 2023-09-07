b61d172d53131fea9e2fc09a12d3ba84ccd73493
mahdi <mahdihadjsassi@fsb.u-carthage.tn>
2023-09-07 10:22:03 +0000

    header and footer developped

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {WelcomeComponent} from "./welcome/welcome.component";
+import {MenuComponent} from "./menu/menu.component";
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', component: WelcomeComponent},
+  {path: 'welcome', pathMatch: 'full', component: WelcomeComponent},
+  {path: 'Menu', component: MenuComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
