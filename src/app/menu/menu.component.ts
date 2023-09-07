6250a27fdcd6c99fe8cefbea3d82b8381ad59191
mahdi <mahdihadjsassi@fsb.u-carthage.tn>
2023-09-07 10:40:39 +0000

    menu displayed

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-
+  menu = [
+    ['Kouskous', 'Tunisian', 'lorem ipsum'],
+    ['Pasta', 'Itialian', 'lorem ipsum'],
+    ['Ma9rouna bel chocolat', 'Kerkennian', 'lorem ipsum'],
+    ['Mlou5ya bel 7out', 'Monastiri', 'lorem ipsum'],
+    ['Slata naya', 'Ben Arous', 'lorem ipsum'],
+    ['M5are9 w zlayba', 'ajeb', 'lorem ipsum']
+  ]
 }
